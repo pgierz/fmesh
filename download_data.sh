29c78869a32f9674636d2ccfe9d6ecf807edc744
Nikolay Koldunov <koldunovn@gmail.com>
2022-10-19 21:46:31 +0200

    typo and uncomment topo download

--- download_data.sh
+++ download_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# curl https://nextcloud.awi.de/s/yNyyKYiifbgbAKT/download/RTopo-2.0.1_30sec_bos_fix_lowres_D3.nc > ./topo/RTopo-2.0.1_30sec_bos_fix_lowres_D3.nc
+curl https://nextcloud.awi.de/s/yNyyKYiifbgbAKT/download/RTopo-2.0.1_30sec_bos_fix_lowres_D3.nc > ./topo/RTopo-2.0.1_30sec_bos_fix_lowres_D3.nc
 curl https://nextcloud.awi.de/s/yDPwoX8iFcLyyf9/download/Archive.zip > ./coastlines/coastlines.zip
 cd coastlines
 unzip coastlines.zip
